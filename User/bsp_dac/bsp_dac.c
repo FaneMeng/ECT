7a86e522dab6096724689bdb973bb28ef56fcfe7
FaneMeng <mengfy1995@hotmail.com>
2019-06-20 16:19:37 +0800

    equil on DAC by shi

--- User/bsp_dac/bsp_dac.c
+++ User/bsp_dac/bsp_dac.c
@@ -9,6 +9,17 @@
   */ 
   
 #include "bsp_dac.h"
+#include "bsp_adc.h"
+
+extern __IO uint16_t ADC_ConvertedValue[NOFCHANEL];
+extern __IO uint8_t equilflag;
+__IO uint16_t Max_A_X = max_value;  //水平路设置最大值
+__IO uint16_t Min_A_X = min_value;    //水平路设置最小值
+__IO uint16_t Max_B_Y = max_value;  //垂直路设置最大值
+__IO uint16_t Min_B_Y = min_value;    //垂直路设置最小值
+
+uint16_t A_set=2000;
+uint16_t B_set=2000;
 
 
 /**
@@ -64,9 +75,125 @@ void DAC_Setdata(uint16_t DAC1Data, uint16_t DAC2Data)
 }
 
 
+/*
+	* @brief   平衡控制程序
+	* @param   X：水平相位信号
+             Y：垂直相位信号
+	* @retval  无
+*/
+/*
+void balance(unsigned short X, unsigned short Y)  //枚举
+{
+	uint16_t X_Value = X & 0xfff;
+	uint16_t Y_Value = Y & 0xfff;
+	uint16_t mid_A_X = (Max_A_X + Min_A_X) >>1;
 
+	if (X_Value>0x7c1 && X_Value<0x83d && Y_Value>0x7c1 && Y_Value<0x83d)
+	{
 
+		 Max_A_X = max_value;
+		 Min_A_X = min_value;
+		 Max_B_Y = max_value;
+		 Min_B_Y = min_value;
+		//
+		equilflag = 0;
+		//
+		return;
+	}
+	
+	if(Max_A_X == Min_A_X)
+	{
+		Max_A_X = max_value;
+		Min_A_X = min_value;
+		
+		if(Min_B_Y < max_value)
+		{
+			Min_B_Y++;
+			DAC_Setdata(mid_A_X,Min_B_Y);
+		}
+		else
+		{
+			equilflag = 0;
+			Min_B_Y=min_value;
+		}
+		return;
+	}
 
+	if (X_Value < 0x83d && Y_Value < 0x83d) Min_A_X = mid_A_X;
+	else if (X_Value > 0x7c1 && Y_Value > 0x7c1) Max_A_X = mid_A_X;   //有出不去的风险
+	else if ((X_Value<0x7c1 && Y_Value>0x83d) || (Y_Value<0x7c1 && X_Value>0x83d))
+	{
+		Max_A_X = max_value;
+		Min_A_X = min_value;
+		//Min_B_Y < Max_B_Y ? (Min_B_Y++) : (equilflag = 0);
+		if(Min_B_Y < max_value)
+		{
+			Min_B_Y++;
+			DAC_Setdata(mid_A_X,Min_B_Y);
+		}
+		else
+		{
+			equilflag = 0;
+			Min_B_Y=min_value;
+		}
+
+		return;
+	}
+
+    DAC_Setdata(mid_A_X,Min_B_Y);
+	//
+	//将参数修改为mid_A_X、mid_B_Y
+	//
+}
+*/
+extern void(*p_Balance_FOR_XY)(unsigned short,unsigned short);
+void balance_A_X(unsigned short X, unsigned short Y)
+{
+	uint16_t X_Value = X & 0xfff;
+	uint16_t Y_Value = Y & 0xfff;
+	if(X_Value<0x7eb)
+	{
+		A_set++;
+		DAC_Setdata(A_set,B_set);
+		p_Balance_FOR_XY=balance_B_Y;
+		return;
+	}
+		if(X_Value>0x813)
+	{
+		A_set--;
+		DAC_Setdata(A_set,B_set);
+		p_Balance_FOR_XY=balance_B_Y;
+		return;
+	}
+	if(X_Value>0x7eb && X_Value<0x813 && Y_Value>0x7eb && Y_Value<0x813)
+	{
+		equilflag = 0;
+	}
+}
+
+void balance_B_Y(unsigned short X, unsigned short Y)
+{
+	uint16_t X_Value = X & 0xfff;
+	uint16_t Y_Value = Y & 0xfff;
+	if(Y_Value<0x7eb)
+	{
+		B_set++;
+		DAC_Setdata(A_set,B_set);
+		p_Balance_FOR_XY=balance_A_X;
+		return;
+	}
+		if(Y_Value>0x813)
+	{
+		B_set--;
+		DAC_Setdata(A_set,B_set);
+		p_Balance_FOR_XY=balance_A_X;
+		return;
+	}
+		if(X_Value>0x7eb && X_Value<0x813 && Y_Value>0x7eb && Y_Value<0x813)
+	{
+		equilflag = 0;
+	}
+}
 
 
 
--- User/bsp_dac/bsp_dac.h
+++ User/bsp_dac/bsp_dac.h
@@ -7,7 +7,10 @@
 
 void DAC_Config(void);
 void DAC_Setdata(uint16_t DAC1Data, uint16_t DAC2DATA);
-
+void balance(uint16_t X, uint16_t Y);
+void balance_A_X(unsigned short, unsigned short);  //声明函数名
+void balance_B_Y(unsigned short, unsigned short);  //声明函数名
+//void(*p_Balance_FOR_XY)(unsigned short,unsigned short);
 
 #endif /* __DAC_H */
 
